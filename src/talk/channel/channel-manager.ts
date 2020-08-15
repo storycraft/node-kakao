f1bf0fc79a0f40eb89218e7b45cc098506cb76a4
unknown <yottabyte090@naver.com>
2020-08-16 00:08:15 +0900

    Update status code

--- src/talk/channel/channel-manager.ts
+++ src/talk/channel/channel-manager.ts
@@ -196,7 +196,7 @@ export class ChannelManager extends IdStore<ChatChannel> {
     protected async requestChannelInfo(channelId: Long): Promise<ChannelInfoStruct | null> {
         let res = await this.client.NetworkManager.requestPacketRes<PacketChannelInfoRes>(new PacketChannelInfoReq(channelId));
 
-        if (res.StatusCode === StatusCode.SUCCESS || res.StatusCode === StatusCode.PARTIAL) {
+        if (res.StatusCode === StatusCode.SUCCESS || res.StatusCode === StatusCode.CHATROOM_NOT_FOUND) {
             return res.ChatInfo!;
         } else {
             return null;
