128ae284d1c34da504f6c33e0932c51b0224b9a0
JellyBrick <shlee1503@naver.com>
2021-02-03 22:55:43 +0900

    Fix 'Require or disallow trailing commas'

--- examples/login-chat.ts
+++ examples/login-chat.ts
@@ -8,39 +8,39 @@
  * Following this example will make automatic response at text "안녕하세요".
  */
 
-import { AuthApiClient, TalkClient } from "node-kakao";
+import { AuthApiClient, TalkClient } from 'node-kakao';
 
 // Supply env variables or replace to value.
-const DEVICE_UUID = process.env['deviceUUID'];
-const DEVICE_NAME = process.env['deviceName'];
+const DEVICE_UUID = process.env['deviceUUID'] as string;
+const DEVICE_NAME = process.env['deviceName'] as string;
 
-const EMAIL = process.env['accountEmail'];
-const PASSWORD = process.env['accountPwd'];
+const EMAIL = process.env['accountEmail'] as string;
+const PASSWORD = process.env['accountPwd'] as string;
 
 const CLIENT = new TalkClient();
 
 CLIENT.on('chat', (data, channel) => {
-    if (data.chat.text === '안녕하세요') {
-        channel.sendChat('안녕하세요');
-    }
+  if (data.chat.text === '안녕하세요') {
+    channel.sendChat('안녕하세요');
+  }
 });
 
 async function main() {
-    const api = await AuthApiClient.create(DEVICE_NAME, DEVICE_UUID);
-    const loginRes = await api.login({
-        email: EMAIL,
-        password: PASSWORD,
+  const api = await AuthApiClient.create(DEVICE_NAME, DEVICE_UUID);
+  const loginRes = await api.login({
+    email: EMAIL,
+    password: PASSWORD,
 
-        // This option force login even other devices are logon
-        forced: true
-    });
-    if (!loginRes.success) throw new Error(`Web login failed with status: ${loginRes.status}`);
+    // This option force login even other devices are logon
+    forced: true,
+  });
+  if (!loginRes.success) throw new Error(`Web login failed with status: ${loginRes.status}`);
 
-    console.log(`Received access token: ${loginRes.result.accessToken}`);
+  console.log(`Received access token: ${loginRes.result.accessToken}`);
 
-    const res = await CLIENT.login(loginRes.result);
-    if (!res.success) throw new Error(`Login failed with status: ${res.status}`);
+  const res = await CLIENT.login(loginRes.result);
+  if (!res.success) throw new Error(`Login failed with status: ${res.status}`);
 
-    console.log('Login success');
+  console.log('Login success');
 }
 main().then();
