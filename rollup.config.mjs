0033cfb25e8aebed1a00348248333ec49df3ad86
storycraft <storycraft@pancake.sh>
2021-02-27 16:11:34 +0900

    Update esm build config. Ignore node builtin polyfill (Done by cdn). Switch to @rollup/plugin-typescript. Disable url import, Preserve modules. Update to 4.0.1

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,29 +5,21 @@
  */
 
 import nodeResolve from '@rollup/plugin-node-resolve';
-import nodePolyfills from 'rollup-plugin-node-polyfills';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default () => {
   const plugins = [
-    commonjs({
-      include: 'node_modules/**',
+    nodeResolve({
+      preferBuiltins: true
     }),
-    nodePolyfills(),
-    nodeResolve(),
+    commonjs(),
     typescript({
-      // bug fix
-      rollupCommonJSResolveHack: true,
-      tsconfigOverride: {
-        compilerOptions: {
-          module: 'ESNext',
-          declaration: false,
-          removeComments: true,
-        },
-      },
-    }),
+      module: 'ESNext',
+      declaration: false,
+      removeComments: true,
+    })
   ];
 
   return [
@@ -37,10 +29,6 @@ export default () => {
         // Excluded
         'axios',
         'form-data',
-
-        // Substituted
-        'bson',
-        'hash-wasm',
       ],
       shimMissingExports: true,
       output: [
@@ -48,11 +36,8 @@ export default () => {
           dir: 'dist_esm',
           format: 'esm',
           sourcemap: true,
-          paths: {
-            'bson': 'https://unpkg.com/bson/dist/bson.browser.esm.js',
-            'hash-wasm': 'https://cdn.jsdelivr.net/npm/hash-wasm/dist/index.esm.min.js',
-            'eventemitter3': 'https://unpkg.com/eventemitter3@latest/umd/eventemitter3.min.js',
-          },
+          preserveModules: true,
+          preserveModulesRoot: 'src'
         },
       ],
       plugins,
